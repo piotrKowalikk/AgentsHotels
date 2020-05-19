7d56ddf9a3fec954ac271817593d4f37b9fe5bb7
Piotr Koskiewicz <piotr888k@gmail.com>
2020-05-19 15:17:26 +0200

    Simple GUI with simple form

---
+++ HotelsGUI/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using HotelsLogic;
+using System;
+using System.Windows;
+
+
+namespace HotelsGUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!DateFrom.SelectedDate.HasValue || !DateTo.SelectedDate.HasValue || !Int32.TryParse(AdultsNumberText.Text, out int adultsNumber))
+                return;
+
+            UserPreference userPreference = new UserPreference()
+            {
+                City = CityTextbox.Text,
+                DateTo = DateFrom.SelectedDate.Value,
+                DateFrom = DateTo.SelectedDate.Value,
+                NumberOfAdults = adultsNumber
+            };
+            BookingSearchService bookingSearchService = new BookingSearchService();
+            bookingSearchService.StartSearch(userPreference);
+        }
+    }
+}
--- HotelsLogic/BookingUserPreference.cs
+++ HotelsLogic/BookingUserPreference.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Text.Json;
 using System.Text.Json.Serialization;
-    
+
 namespace HotelsLogic
 {
     class BookingUserPreference

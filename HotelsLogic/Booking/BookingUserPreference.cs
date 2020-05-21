406c65944474514bac66e2d7b0dcebeb3f7b1544
Piotr Kowalik <kowalikk.piotr@gmail.com>
2020-05-21 22:08:23 +0200

    added all handled properties in robot

--- HotelsLogic/Booking/BookingUserPreference.cs
+++ HotelsLogic/Booking/BookingUserPreference.cs
@@ -8,14 +8,30 @@ namespace HotelsLogic
 {
     class BookingUserPreference
     {
-        private UserPreference userPreference;
+        public string dateFrom { get; private set; }
+        public string dateTo { get; private set; }
+        public int numbeOfAdults { get; private set; }
+        public int numberOfChildren { get; private set; }
+        public int numberOfRooms { get; private set; }
+        public int fromPrice { get; private set; }
+        public int toPrice { get; private set; }
+        public string city { get; private set; }
+
         public BookingUserPreference(UserPreference userPreference)
         {
-            this.userPreference = userPreference;
+            dateFrom = userPreference.DateFrom.Year + "-" + userPreference.DateFrom.Month + "-" + userPreference.DateFrom.Day;
+            dateTo = userPreference.DateTo.Year + "-" + userPreference.DateTo.Month + "-" + userPreference.DateTo.Day;
+            numbeOfAdults = userPreference.NumberOfAdults;
+            numberOfChildren = 2;
+            numberOfRooms = 2;//TODO
+            fromPrice = 300;
+            toPrice = 500;
+            city = userPreference.City;
         }
+
         public string GetDeserializedUserPreference()
         {
-            return JsonSerializer.Serialize(userPreference);
+            return JsonSerializer.Serialize(this);
         }
     }
 }

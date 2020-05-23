64c91eff4e4db172ab0625a4b776e3aa5d8aa967
Piotr Koskiewicz <piotr888k@gmail.com>
2020-05-23 23:41:14 +0200

    Prepare trivago user preferences

--- HotelsLogic/Trivago/TrivagoUserPreference.cs
+++ HotelsLogic/Trivago/TrivagoUserPreference.cs
@@ -4,16 +4,26 @@ namespace HotelsLogic
 {
     class TrivagoUserPreference
     {
-        private UserPreference userPreference;
+        public string dateFrom { get; private set; }
+        public string dateTo { get; private set; }
+        public int numberOfAdults { get; private set; }
+        public string city { get; private set; }
+        public string outputPath { get; private set; }
+        public int delay { get; private set; }
 
         public TrivagoUserPreference(UserPreference userPreference)
         {
-            this.userPreference = userPreference;
+            dateFrom = $"{userPreference.DateFrom.Month}/{userPreference.DateFrom.Day}/{userPreference.DateFrom.Year}";
+            dateTo = $"{userPreference.DateTo.Month}/{userPreference.DateTo.Day}/{userPreference.DateTo.Year}";
+            numberOfAdults = userPreference.NumberOfAdults;
+            city = userPreference.City;
+            outputPath = ResultService.ResultsPath;
+            delay = userPreference.Delay;
         }
 
         public string GetDeserializedUserPreference()
         {
-            return JsonSerializer.Serialize(userPreference);
+            return JsonSerializer.Serialize(this);
         }
     }
 }

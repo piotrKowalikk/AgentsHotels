d9e2a965e4cba87556a0f88bfe236d2870a0d9f7
Piotr Kowalik <kowalikk.piotr@gmail.com>
2020-07-16 07:00:41 +0200

    added default filters to booking

--- HotelsLogic/Booking/BookingUserPreference.cs
+++ HotelsLogic/Booking/BookingUserPreference.cs
@@ -19,6 +19,24 @@ namespace HotelsLogic
         public int fromPrice { get; private set; }
         public int toPrice { get; private set; }
         public string city { get; private set; }
+        public int numberOfReturnOffers { get; set; }
+        public int delay { get; set; }
+        //filtry
+        public bool airConditioning { get; private set; }
+        public bool freeCancelation { get; private set; }
+        public bool wifi { get; private set; }
+        public bool bar { get; private set; }
+        public bool pool { get; private set; }
+        public bool fridge { get; private set; }
+        public bool microwave { get; private set; }
+        public bool safe { get; private set; }
+        public bool tv { get; private set; }
+        public bool massage { get; private set; }
+        public bool sauna { get; private set; }
+        public bool gym { get; private set; }
+        public bool spa { get; private set; }
+        public bool stars { get; private set; }
+
         public string outputPath { get; private set; }
 
         public BookingUserPreference(UserPreference userPreference)
@@ -35,6 +53,21 @@ namespace HotelsLogic
             dateFromMonthsToSkip = userPreference.DateFrom.Month - DateTime.Now.Month;
             dateToMonthsToSkip = userPreference.DateTo.Month - DateTime.Now.Month;
             outputPath = ResultService.ResultsPath;
+            //filters
+            airConditioning = false;
+            freeCancelation = false;
+            wifi = false;
+            bar = false;
+            pool = false;
+            fridge = false;
+            microwave = false;
+            safe = false;
+            tv = false;
+            massage = false;
+            sauna = false;
+            gym = false;
+            spa = false;
+            stars = false;
 
         }
         private static string MakeTwoPostions(int num)
